996eda8ba808118638e68321d844e2ba5dc03829
Sunny Guan <sunny.peng.guan@gmail.com>
2021-03-21 12:19:19 -0700

    fixed dnd drop zone and colors

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -34,11 +34,11 @@ export function Dashboard() {
   const highlighter = (e) => {
     let text = e.target.value;
     let enter = e.key === "Enter";
-    let [board, info, selected, resetFlag] = process(text, enter, board);
+    let [newBoard, info, selected, resetFlag] = process(text, enter, board);
     setInfo(info);
     setSelected(selected);
-    setBoard(board);
-    ls.set("board", board);
+    setBoard(newBoard);
+    ls.set("board", newBoard);
     if (resetFlag === 1 || resetFlag === 2) reset();
     if (resetFlag === 2) setInputValue("");
   };
@@ -59,6 +59,13 @@ export function Dashboard() {
       setBoard(newBoard);
       ls.set("board", newBoard);
     }
+  };
+
+  const getBackgroundColor = (key, snapshot) => {
+    if (key.toLowerCase() === selected[1].toLowerCase()) return "bg-green-200";
+    if (snapshot.isDraggingOver) return "bg-green-200";
+    if (snapshot.draggingFromThisWith) return "bg-indigo-200";
+    return "bg-blue-100";
   };
 
   return (
@@ -89,27 +96,26 @@ export function Dashboard() {
         <div className="px-8 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-4 text-center">
           <DragDropContext onDragEnd={handleOnDragEnd}>
             {Object.keys(board).map((key) => (
-              <div
-                className={
-                  "rounded-xl shadow-xl p-4 " +
-                  (key.toLowerCase() === selected[1].toLowerCase()
-                    ? "bg-green-200"
-                    : "bg-blue-100")
-                }
-              >
-                <div className="flex">
-                  <h1 className="text-2xl font-medium pb-4 flex-1 text-left">
-                    {key}
-                  </h1>
-                  <h1 className="text-2xl ">{board[key].length}</h1>
-                </div>
-
-                <Droppable droppableId={key}>
-                  {(provided) => (
+              <Droppable droppableId={key}>
+                {(provided, snapshot) => (
+                  <div
+                    className={
+                      "rounded-xl shadow-xl p-4 flex flex-col " +
+                      getBackgroundColor(key, snapshot)
+                    }
+                  >
+                    <div className="">
+                      <div className="flex">
+                        <h1 className="text-2xl font-medium pb-4 flex-1 text-left">
+                          {key}
+                        </h1>
+                        <h1 className="text-2xl ">{board[key].length}</h1>
+                      </div>
+                    </div>
                     <div
                       {...provided.droppableProps}
                       ref={provided.innerRef}
-                      className="grid grid-cols-1 gap-4"
+                      className="grid grid-cols-1 gap-4 flex-1"
                     >
                       {Array.from(board[key]).map((company, id) => (
                         <Draggable
@@ -139,9 +145,9 @@ export function Dashboard() {
                       ))}
                       {provided.placeholder}
                     </div>
-                  )}
-                </Droppable>
-              </div>
+                  </div>
+                )}
+              </Droppable>
             ))}
           </DragDropContext>
         </div>
