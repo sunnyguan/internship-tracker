6b4d5bd0feba0e0323f12bcd53ff83a9985610af
Sunny Guan <sunny.peng.guan@gmail.com>
2021-03-30 21:15:50 -0700

    added footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
-    <div className="p-4 text-center bg-blue-100 grid grid-cols-2 divide-x-2 divide-blue-400">
-      <div className="text-right px-4">
+    <div className="p-4 text-center bg-blue-100 grid grid-cols-1 md:grid-cols-3 md:divide-x-2 divide-blue-400">
+      <div className="px-4">
         Check out the project on{" "}
         <a
           href="https://github.com/sunnyguan/internship-tracker"
@@ -11,13 +11,21 @@ function Footer() {
         </a>{" "}
       </div>
 
-      <div className="text-left px-4">
+      <div className="px-4">
         Made by{" "}
         <a
           href="https://www.linkedin.com/in/sunny-guan/"
           className="underline text-blue-700"
         >
           Sunny Guan
+        </a>
+      </div>
+      <div className="px-4">
+        View the <a
+          href="https://github.com/sunnyguan/internship-tracker#features"
+          className="underline text-blue-700"
+        >
+          features list
         </a>
       </div>
     </div>
